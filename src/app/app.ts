03c5069032307807ad2be2e6a7f72ff2289bec13
noahwhygodwhy <noahwhygodwhy@pm.me>
2026-08-15 20:21:30 -0400

    random centered grid

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,40 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Grid, GridRow, GridCell, GridCellWidget} from '@angular/aria/grid';
 
- import {MatCardModule} from '@angular/material/card';
+import {MatCardModule} from '@angular/material/card';
+
+function shuffle(array : DataEntry[]) : DataEntry[] {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+
+export interface MatchEntry
+{
+  index : number;
+  name : string;
+  description : string;
+  color : string; // todo: is this right?
+}
+export interface DataEntry
+{
+  title : string;
+  description : string;
+  tags :string[];
+  image: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -10,8 +42,12 @@ import {Grid, GridRow, GridCell, GridCellWidget} from '@angular/aria/grid';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('affiliations');
+
+
+  // affiliationEntries = [
+  // ];
 
   dataEntries : DataEntry[] = [
     {title:"titlea1", description: "description a1", tags: ["a", "b", "c", "d"], image: "www.ahhhh.com"},
@@ -30,16 +66,12 @@ export class App {
     {title:"titleb4", description: "description b1", tags: ["a", "b", "c", "d"], image: "www.ahhhh.com"},
     {title:"titlec4", description: "description c1", tags: ["a", "b", "c", "d"], image: "www.ahhhh.com"},
     {title:"titled4", description: "description d1", tags: ["a", "b", "c", "d"], image: "www.ahhhh.com"},
-  ]
+  ];
+
+  ngOnInit()
+  {
+    this.dataEntries = shuffle(this.dataEntries);
+  }
 }
 
 
-
-export interface DataEntry
-{
-  title : string;
-  description : string;
-  tags :string[];
-  image: string;
-
-}
