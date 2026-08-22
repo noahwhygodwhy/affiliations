355fde4f9449707f0a6a8ed458d38037921933e8
noahwhygodwhy <noahwhygodwhy@pm.me>
2026-08-22 12:50:32 -0400

    attempting different cron setup?

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
 
-interface Env {}
+interface Env {
+	TestSecret:string; // todo; is this the right thing?
+}
 export default {
 	async scheduled(
 		controller: ScheduledController,
@@ -14,5 +16,6 @@ export default {
 		ctx: ExecutionContext,
 	) {
 		console.log("cron processed");
+		console.log("testing a fake secret access:", env.TestSecret);
 	},
 };
