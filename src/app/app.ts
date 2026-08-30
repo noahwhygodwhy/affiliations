bc1f6a28d77e7d0e2a7617b56fdcf67347c74078
noahwhygodwhy <noahwhygodwhy@pm.me>
2026-08-29 22:54:34 -0400

    oh my god working animations

--- src/app/app.ts
+++ src/app/app.ts
@@ -26,12 +26,10 @@ export interface DataEntry
     oldX : number;
     oldY : number;
     currentlyPressed:boolean;
-    oldIDForAnimation : number;
 }
 
 async function LoadTodaysEntry()
 {
-    console.log("top of LoadTodaysEntry");
     let resp = await fetch("/fetchtodaysids",
         {
             method:"GET",
@@ -41,11 +39,7 @@ async function LoadTodaysEntry()
             ]
         }
     )
-    console.log(resp);
-    console.log("did fetch");
     let data: DatabaseRow[] = await resp.json<DatabaseRow[]>();
-    console.log("awaited on json");
-    console.log(data);
 
     let fetchedDataEntries: DataEntry[] = data.map((d:DatabaseRow, index:number):DataEntry => {
         return {
@@ -59,11 +53,8 @@ async function LoadTodaysEntry()
             oldX:0,
             oldY:0,
             currentlyPressed:false,
-            oldIDForAnimation:index
         }
     });
-    console.log("prepared data entries");
-    console.log(fetchedDataEntries);
     return fetchedDataEntries;
 }
 
@@ -80,6 +71,8 @@ export class App implements OnInit, AfterViewInit{
 
     @Output() overlayModalData:DataEntry|undefined = undefined;
 
+    // layer of indirection makes the animation hopefully easy
+    dataIndices : number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     dataEntries : DataEntry[] = []
 
     has4ButtonsSelected:boolean = false;
@@ -95,29 +88,29 @@ export class App implements OnInit, AfterViewInit{
 
     shuffle()
     {
+
         let minimumIndexToTouch = this.numMatchesMade*4;
         let currentIndex = this.dataEntries.length;
 
         while (currentIndex != minimumIndexToTouch)
-            {
+        {
             let lengthOfRange = currentIndex - minimumIndexToTouch;
 
             let randomIndex = Math.floor(Math.random() * lengthOfRange) + minimumIndexToTouch;
             currentIndex--;
 
-            let tempA = this.dataEntries[currentIndex];
-            let tempB = this.dataEntries[randomIndex];
+            let tempA = this.dataIndices[currentIndex];
+            let tempB = this.dataIndices[randomIndex];
 
-            this.dataEntries[currentIndex] = {...tempB};
-            this.dataEntries[randomIndex] = {...tempA};
+            this.dataIndices[currentIndex] = tempB;
+            this.dataIndices[randomIndex] = tempA;
         }
     }
 
     shuffleEntries()
     {
-        console.log("shuffling");
-        this.shuffle();
         this.recordPositions()
+        this.shuffle();
         this.tentativeAnimationThing();
 
     }
@@ -126,103 +119,99 @@ export class App implements OnInit, AfterViewInit{
     async ngOnInit()
     {
         this.overlayModalData = undefined;
-        console.log("user agent?:", navigator.userAgent);
-        console.log("ngoninit");
         this.dateString = new Date().toISOString().split("T")[0]
-        console.log("doing the load");
         LoadTodaysEntry().then((data)=> {
             this.dataEntries = data;
             this.ready = true;
-            console.log("ready:", this.ready);
             this.shuffle();
-            // this.tentativeAnimationThing();
-            // this.ref.markForCheck();
             this.ref.detectChanges();
 
             this.recordInitialPositions(); });
-
-        console.log("ready:", this.ready);
     }
 
     ngAfterViewInit()
     {
-        console.log("ngafterviewinit")
     }
 
     recordInitialPositions()
     {
-        console.log("record initial positions");
-        for(let i = 0; i < this.dataEntries.length; i++)
+        for(let elementIndex = 0; elementIndex < this.dataEntries.length; elementIndex++)
             {
-            let boxI:HTMLElement|null = document.getElementById("square"+i);
+            let boxI:HTMLElement|null = document.getElementById("square"+elementIndex);
             if(boxI != null)
                 {
                 let d : DOMRect = boxI.getBoundingClientRect();
-                this.dataEntries[i].oldX = d.left;
-                this.dataEntries[i].oldY = d.top;
+                this.dataEntries[this.dataIndices[elementIndex]].oldX = d.left;
+                this.dataEntries[this.dataIndices[elementIndex]].oldY = d.top;
             }
             else
                 {
-                console.error("square"+i, "is null")
+                console.error("square"+elementIndex, "is null")
             }
-        };
+        }
     }
 
     recordPositions()
     {
-        console.log("record positions");
-        for(let i = 0; i < this.dataEntries.length; i++)
+        for(let elementIndex = 0; elementIndex < this.dataEntries.length; elementIndex++)
         {
-            this.dataEntries[i].oldIDForAnimation=i;
-            let boxI:HTMLElement|null = document.getElementById("square"+i);
+            let boxI:HTMLElement|null = document.getElementById("square"+elementIndex);
             if(boxI != null)
                 {
                 let d : DOMRect = boxI.getBoundingClientRect();
-
-                let deltaX = this.dataEntries[i].oldX -  d.left;
-                let deltaY =  this.dataEntries[i].oldY - d.top;
-
-                boxI.style.transition = 'none';
-                boxI.style.transform = "translate(" + deltaX + "px, " + deltaY + "px)";
-
-                this.ref.detectChanges();
-                // void boxI.offsetWidth;
-
-                this.dataEntries[i].oldX = d.left;
-                this.dataEntries[i].oldY = d.top;
+                this.dataEntries[this.dataIndices[elementIndex]].oldX = d.left;
+                this.dataEntries[this.dataIndices[elementIndex]].oldY = d.top;
             }
             else
                 {
-                console.error("square"+i, "is null")
+                console.error("square"+elementIndex, "is null")
             }
         };
     }
 
     tentativeAnimationThing()
     {
-        console.log("tentativeAnimationThing");
         requestAnimationFrame(() => {
-            for(let i = 0; i < 16; i++)
+            for(let elementIndex = 0; elementIndex < 16; elementIndex++)
             {
-                let oldId = this.dataEntries[i].oldIDForAnimation;
-                let boxI:HTMLElement|null = document.getElementById("square"+oldId);
+                let boxI:HTMLElement|null = document.getElementById("square"+elementIndex);
                 if(boxI != null)
-                    {
-                    boxI.style.transition = 'transform 300ms ease';
-                    boxI.style.transform = '';
+                {
+                    let dataIndex = this.dataIndices[elementIndex];
+
+                    let d = boxI.getBoundingClientRect();
+
+                    let deltaX = this.dataEntries[dataIndex].oldX - d.left;
+                    let deltaY = this.dataEntries[dataIndex].oldY - d.top;
+
+                    boxI.style.transition = "none";
+                    boxI.style.transform =
+                        `translate(${deltaX}px, ${deltaY}px)`;
+
+                    requestAnimationFrame(()=> {
+                        let boxJ:HTMLElement|null = document.getElementById("square"+elementIndex);
+                        if(boxJ != null)
+                        {
+                            boxJ.style.transition = "transform 300ms ease";
+                            boxJ.style.transform = "";
+                        }
+                        else
+                        {
+                            console.error("square"+elementIndex, "is null")
+                        }
+                    });
+
                 }
                 else
-                    {
-                    console.error("square"+oldId, "is null")
+                {
+                    console.error("square"+elementIndex, "is null")
                 }
-                this.dataEntries[i].oldIDForAnimation = i;
             };
         });
     }
 
     ngOnChanges()
     {
-        console.log("ngOnChanges");
     }
 
     countSelected():number
@@ -238,41 +227,41 @@ export class App implements OnInit, AfterViewInit{
     trySelect(index:number) : void
     {
         this.justTriedBadMatch = false;
-        for(let i = 0; i < this.dataEntries.length; i++)
+        for(let elementIndex = 0; elementIndex < this.dataEntries.length; elementIndex++)
+        {
+            if(this.dataEntries[this.dataIndices[elementIndex]].selected)
             {
-            if(this.dataEntries[i].selected)
-                {
-                let boxI:HTMLElement|null = document.getElementById("square"+i);
+                let boxI:HTMLElement|null = document.getElementById("square"+elementIndex);
                 if(boxI != null)
-                    {
+                {
                     // this done here instead of a timeout after setting the class is because SetTimeout doesn't seem to work on mobile? idk
                     boxI.className = boxI.className.replace(" wigglyButtonCauseYoureDum","");
                 }
                 else
                     {
-                    console.error("square"+i, "is null")
+                    console.error("square"+elementIndex, "is null")
                 }
             }
         }
 
         let numselected:number = this.countSelected();
 
-        if(this.dataEntries[index].matched)
-            {
+        if(this.dataEntries[this.dataIndices[index]].matched)
+        {
             return;
         }
 
-        let isAlreadySelected : boolean = this.dataEntries[index].selected;
+        let isAlreadySelected : boolean = this.dataEntries[this.dataIndices[index]].selected;
 
         if(isAlreadySelected)
-            {
-            this.dataEntries[index].selected = false;
+        {
+            this.dataEntries[this.dataIndices[index]].selected = false;
             numselected--;
             this.has4ButtonsSelected = false;
         }
         else if (numselected < 4)
-            {
-            this.dataEntries[index].selected = true;
+        {
+            this.dataEntries[this.dataIndices[index]].selected = true;
             numselected++;
             if(numselected == 4)
                 {
@@ -284,7 +273,7 @@ export class App implements OnInit, AfterViewInit{
     deselectAll():void
     {
 
-        this.dataEntries.forEach((data:DataEntry)=> {
+        this.dataEntries.forEach((data:DataEntry) => {
             data.selected = false;
         });
         this.has4ButtonsSelected = false;
@@ -295,16 +284,17 @@ export class App implements OnInit, AfterViewInit{
 
         let currMatchPick:number = -1;
 
+
         for(let i = 0; i < this.dataEntries.length; i++)
-            {
+        {
             if(this.dataEntries[i].selected)
-                {
+            {
                 if(currMatchPick==-1)
-                    {
+                {
                     currMatchPick = this.dataEntries[i].matchGroupIndex;
                 }
                 else
-                    {
+                {
                     if(this.dataEntries[i].matchGroupIndex != currMatchPick) // TODO: refactor to allow for a "close guess" hint?
                     {
                         currMatchPick=-1;
@@ -316,15 +306,15 @@ export class App implements OnInit, AfterViewInit{
         }
 
         if(currMatchPick == -1)
-            {
+        {
             this.justTriedBadMatch = true;
             for(let i = 0; i < this.dataEntries.length; i++)
                 {
-                if(this.dataEntries[i].selected)
+                if(this.dataEntries[this.dataIndices[i]].selected)
                     {
                     let boxI:HTMLElement|null = document.getElementById("square"+i);
                     if(boxI != null)
-                        {
+                    {
                         // does this work
                         boxI.className = boxI.className.replace(" wigglyButtonCauseYoureDum","");
                         boxI.className = boxI.className + " wigglyButtonCauseYoureDum";
@@ -351,8 +341,9 @@ export class App implements OnInit, AfterViewInit{
 
         this.recordPositions()
 
-        this.dataEntries.sort((dataA:DataEntry, dataB:DataEntry) => {
-            // console.log("comparing", dataA, dataB);
+        this.dataIndices.sort((dataIndexA:number, dataIndexB:number) => {
+            let dataA = this.dataEntries[dataIndexA];
+            let dataB = this.dataEntries[dataIndexB];
             if(dataA.matched != dataB.matched)
                 {
                 return dataA.matched < dataB.matched ? 1 : -1;
@@ -380,24 +371,22 @@ export class App implements OnInit, AfterViewInit{
         this.numMatchesMade +=1;
 
         if(this.numMatchesMade == 4)
-            {
-            // todo; handle win?
+        {
             this.won = true;
-            console.log("win omg yoou did it wooooooooooo?");
         }
     }
 
-    getx44ButtonClass(index:number):string
+    getx44ButtonClass(elementIndex:number):string
     {
         let retVal = ""
 
-        if(this.dataEntries[index].matched)
-            {
-            retVal += "matchedx44ButtonGroup" + this.dataEntries[index].matchGroupIndex;
+        if(this.dataEntries[this.dataIndices[elementIndex]].matched)
+        {
+            retVal += "matchedx44ButtonGroup" + this.dataEntries[this.dataIndices[elementIndex]].matchGroupIndex;
         }
         else
         {
-            if(this.dataEntries[index].selected)
+            if(this.dataEntries[this.dataIndices[elementIndex]].selected)
             {
                 retVal += "selectedx44Button";
             }
@@ -405,7 +394,7 @@ export class App implements OnInit, AfterViewInit{
             {
                 retVal += "inactivex44Button";
             }
-            if(this.dataEntries[index].currentlyPressed)
+            if(this.dataEntries[this.dataIndices[elementIndex]].currentlyPressed)
             {
                 retVal += " pressedx44Button";
             }
@@ -416,34 +405,31 @@ export class App implements OnInit, AfterViewInit{
 
     timeoutId:number = -1;
 
-    pointerDown(valueIndex:number)
+    pointerDown(elementIndex:number)
     {
-        this.startModalCountdown(valueIndex);
-        this.dataEntries[valueIndex].currentlyPressed = true;
-        console.log("setting currentlyPressed on button ", valueIndex, "to true");
+        this.startModalCountdown(elementIndex);
+        this.dataEntries[this.dataIndices[elementIndex]].currentlyPressed = true;
     }
 
-    pointerUp(valueIndex:number)
+    pointerUp(elementIndex:number)
     {
         this.stopModalCountdown();
-        console.log("setting currentlyPressed on button ", valueIndex, "to false");
-        this.dataEntries[valueIndex].currentlyPressed = false;
+        this.dataEntries[this.dataIndices[elementIndex]].currentlyPressed = false;
     }
 
-    startModalCountdown(valueIndex:number)
+    startModalCountdown(elementIndex:number)
     {
         this.stopModalCountdown();
         let milisecondsToWaitForModal:number = 500;
-        console.log("starting modal countdown")
-        this.timeoutId = setTimeout(()=>{console.log("setting modal data"); this.overlayModalData = this.dataEntries[valueIndex]; this.ref.detectChanges()}, milisecondsToWaitForModal);
+        this.timeoutId = setTimeout(()=>{
+                this.overlayModalData = this.dataEntries[this.dataIndices[elementIndex]];
+                this.ref.detectChanges()},
+            milisecondsToWaitForModal);
     }
     stopModalCountdown()
     {
-        console.log("ending modal countdown")
         if(this.timeoutId === -1)
         {
-
-            console.log("except it's -1")
             return;
         }
         clearTimeout(this.timeoutId);
